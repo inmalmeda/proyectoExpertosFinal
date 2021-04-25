4fc2b53e9c4c45b42636c390cc86216b6b62884e
Inma Jimenez <inmalmeda@hotmail.com>
2021-04-25 19:55:17 +0200

    Arreglos heroku8

--- src/main/java/com/inmajimenez/proyectoFinal/controller/ExpertController.java
+++ src/main/java/com/inmajimenez/proyectoFinal/controller/ExpertController.java
@@ -20,7 +20,7 @@ import java.net.URISyntaxException;
  */
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "https://projectfrontinma-5ju2tjiv8-inmalmeda.vercel.app", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
+@CrossOrigin(origins = "https://projectfrontinma-jg1b096qy-inmalmeda.vercel.app", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
 public class ExpertController {
 
     private final ExpertService expertService;
