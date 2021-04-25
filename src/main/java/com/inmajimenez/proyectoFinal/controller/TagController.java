9ac3a1cc3ecd6c91fcd2dfe7985ffaa9945c9d7f
Inma Jimenez <inmalmeda@hotmail.com>
2021-04-25 20:41:43 +0200

    Arreglos heroku9

--- src/main/java/com/inmajimenez/proyectoFinal/controller/TagController.java
+++ src/main/java/com/inmajimenez/proyectoFinal/controller/TagController.java
@@ -22,7 +22,7 @@ import java.net.URISyntaxException;
  */
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "https://finalprojectfrontinma-c2mf3w5vz-inmalmeda.vercel.app", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
+@CrossOrigin(origins = "https://finalprojectfrontinma-bwznjp3lw-inmalmeda.vercel.app", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
 public class TagController {
 
     private final TagService tagService;
