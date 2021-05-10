6427e2e211cf8218f0afbf39159989c3494d389f
Inma Jimenez <inmalmeda@hotmail.com>
2021-05-10 17:33:52 +0200

    ConfigCors2

--- src/main/java/com/inmajimenez/proyectoFinal/controller/UserController.java
+++ src/main/java/com/inmajimenez/proyectoFinal/controller/UserController.java
@@ -15,8 +15,8 @@ import java.net.URISyntaxException;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:4200", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
-//@CrossOrigin(origins = "https://ijimenezfinal.vercel.app", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
+//@CrossOrigin(origins = "http://localhost:4200", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
+@CrossOrigin(origins = "https://ijimenezfinal.vercel.app", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
 public class UserController {
 
     UserService userService;
