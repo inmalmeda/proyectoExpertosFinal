d1bc17c50070d2e5d6e82b79d09857bb98c5934b
Inma Jimenez <inmalmeda@hotmail.com>
2021-05-10 17:22:16 +0200

    ConfigCors

--- src/main/java/com/inmajimenez/proyectoFinal/ProyectoFinalApplication.java
+++ src/main/java/com/inmajimenez/proyectoFinal/ProyectoFinalApplication.java
@@ -2,6 +2,9 @@ package com.inmajimenez.proyectoFinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Proyecto desarrollado por: Inmaculada Jiménez Almeda
@@ -13,4 +16,13 @@ public class ProyectoFinalApplication {
 		SpringApplication.run(ProyectoFinalApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("").allowedMethods("GET", "POST","PUT", "DELETE");
+			}
+		};
+	}
 }
